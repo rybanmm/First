d079f5f9e8becfb050adc486dcb5bb88c7dd51c3
Mihail <rybanmm@gmail.com>
2021-11-23 14:12:34 +0200

    Add CalculatorViewController

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Михаил Рубан on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func calculator() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nibBundle)
+        let calculator = storyboard.instantiateViewController(withIdentifier: "CalculatorViewController")
+        present(calculator, animated: true, completion: nil)
+    }
+    
+}
+
